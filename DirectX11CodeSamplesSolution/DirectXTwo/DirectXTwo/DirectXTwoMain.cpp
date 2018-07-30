59b16f9b7c8486a144b4234be4ab968d42e4b8b0
pc <pc@DESKTOP-RDFTR3A>
2018-07-30 23:51:16 +0800

    DirectX11在窗口绘制一个三角形

---
+++ DirectX11CodeSamplesSolution/DirectXTwo/DirectXTwo/DirectXTwoMain.cpp
@@ -0,0 +1,348 @@
+//--------------------------------------------------------------------------------------
+// File: DirectXTwoMain.cpp
+//
+// 绘制一个三角形
+//
+//--------------------------------------------------------------------------------------
+
+//#include <windows.h>
+//#include <d3d11_1.h>
+//#include <d3dcompiler.h>
+//#include <directxmath.h>
+//#include <directxcolors.h>
+
+#include "DXUT.h"
+
+#pragma warning( disable : 4100 )
+
+using namespace DirectX;
+
+#pragma comment( lib, "comctl32.lib" )
+
+
+ID3D11Buffer * g_pVertexBuffer = nullptr;
+ID3D11VertexShader * g_pVertexShader = nullptr;
+ID3D11PixelShader * g_pPixelShader = nullptr;
+ID3D11InputLayout * g_pInputLayout = nullptr;
+
+//--------------------------------------------------------------------------------------
+// Structures
+//--------------------------------------------------------------------------------------
+struct SimpleVertex
+{
+	XMFLOAT3 Pos;
+};
+
+
+
+
+//--------------------------------------------------------------------------------------
+// Helper for compiling shaders with D3DCompile
+//
+// With VS 11, we could load up prebuilt .cso files instead...
+// 此处要加载d3dcompiler.lib库
+//--------------------------------------------------------------------------------------
+HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut)
+{
+	HRESULT hr = S_OK;
+
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#ifdef _DEBUG
+	// Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+	// Setting this flag improves the shader debugging experience, but still allows 
+	// the shaders to be optimized and to run exactly the way they will run in 
+	// the release configuration of this program.
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+
+	// Disable optimizations to further improve shader debugging
+	dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+	//此处要加载d3dcompiler.lib库
+	ID3DBlob* pErrorBlob = nullptr;
+	hr = D3DCompileFromFile(szFileName, nullptr, nullptr, szEntryPoint, szShaderModel,
+		dwShaderFlags, 0, ppBlobOut, &pErrorBlob);
+	if (FAILED(hr))
+	{
+		if (pErrorBlob)
+		{
+			OutputDebugStringA(reinterpret_cast<const char*>(pErrorBlob->GetBufferPointer()));
+			pErrorBlob->Release();
+		}
+		return hr;
+	}
+	if (pErrorBlob) pErrorBlob->Release();
+
+	return S_OK;
+}
+
+
+
+//--------------------------------------------------------------------------------------
+// Reject any D3D11 devices that aren't acceptable by returning false
+//--------------------------------------------------------------------------------------
+bool CALLBACK IsD3D11DeviceAcceptable(const CD3D11EnumAdapterInfo *AdapterInfo, UINT Output, const CD3D11EnumDeviceInfo *DeviceInfo,
+	DXGI_FORMAT BackBufferFormat, bool bWindowed, void* pUserContext)
+{
+	return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Called right before creating a D3D device, allowing the app to modify the device settings as needed
+//--------------------------------------------------------------------------------------
+bool CALLBACK ModifyDeviceSettings(DXUTDeviceSettings* pDeviceSettings, void* pUserContext)
+{
+	return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Create any D3D11 resources that aren't dependant on the back buffer
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D11CreateDevice(ID3D11Device* pd3dDevice, const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc,
+	void* pUserContext)
+{
+	HRESULT hr = S_OK;
+
+
+	// Compile the vertex shader
+	ID3DBlob* pVSBlob = nullptr;
+	hr = CompileShaderFromFile(L"Tutorial02.fx", "VS", "vs_4_0", &pVSBlob);
+	if (FAILED(hr))
+	{
+		MessageBox(nullptr,
+			L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+		return hr;
+	}
+
+	// Create the vertex shader
+	hr = pd3dDevice->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), nullptr, &g_pVertexShader);
+	if (FAILED(hr))
+	{
+		pVSBlob->Release();
+		return hr;
+	}
+
+
+
+
+
+	ID3D11DeviceContext * g_pImmediateContext = DXUTGetD3D11DeviceContext();
+
+	// Define the input layout
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	UINT numElements = ARRAYSIZE(layout);
+
+	// Create the input layout
+	hr = pd3dDevice->CreateInputLayout(layout, numElements, pVSBlob->GetBufferPointer(),
+		pVSBlob->GetBufferSize(), &g_pInputLayout);
+	pVSBlob->Release();
+	if (FAILED(hr))
+		return hr;
+
+	// Set the input layout
+	g_pImmediateContext->IASetInputLayout(g_pInputLayout);
+
+
+
+
+
+
+	// Compile the pixel shader
+	ID3DBlob* pPSBlob = nullptr;
+	hr = CompileShaderFromFile(L"Tutorial02.fx", "PS", "ps_4_0", &pPSBlob);
+	if (FAILED(hr))
+	{
+		MessageBox(nullptr,
+			L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+		return hr;
+	}
+
+	// Create the pixel shader
+	hr = pd3dDevice->CreatePixelShader(pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), nullptr, &g_pPixelShader);
+	pPSBlob->Release();
+	if (FAILED(hr))
+		return hr;
+
+
+	//
+
+	// Create vertex buffer
+	SimpleVertex vertices[] =
+	{
+		{XMFLOAT3(0.0f, 0.25f, 0.5f)},
+		{XMFLOAT3(0.25f, -0.25f,0.5f)},
+		{XMFLOAT3(-0.25f, -0.25f,0.5f)},
+	};
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(SimpleVertex) * 3;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	D3D11_SUBRESOURCE_DATA InitData;
+	ZeroMemory(&InitData, sizeof(InitData));
+	InitData.pSysMem = vertices;
+	hr = pd3dDevice->CreateBuffer(&bd, &InitData, &g_pVertexBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	// Set vertex buffer
+	UINT stride = sizeof(SimpleVertex);
+	UINT offset = 0;
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+	// Set primitive topology
+	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+
+
+	return S_OK;
+}
+
+//--------------------------------------------------------------------------------------
+// Create any D3D11 resources that depend on the back buffer
+//--------------------------------------------------------------------------------------
+HRESULT CALLBACK OnD3D11ResizedSwapChain(ID3D11Device* pd3dDevice, IDXGISwapChain* pSwapChain,
+	const DXGI_SURFACE_DESC* pBackBufferSurfaceDesc, void* pUserContext)
+{
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle updates to the scene.  This is called regardless of which D3D API is used
+//--------------------------------------------------------------------------------------
+void CALLBACK OnFrameMove(double fTime, float fElapsedTime, void* pUserContext)
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render the scene using the D3D11 device
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D11FrameRender(ID3D11Device* pd3dDevice, ID3D11DeviceContext* pd3dImmediateContext,
+	double fTime, float fElapsedTime, void* pUserContext)
+{
+	// Clear render target and the depth stencil 
+	auto pRTV = DXUTGetD3D11RenderTargetView();
+	pd3dImmediateContext->ClearRenderTargetView(pRTV, Colors::MidnightBlue);
+
+	auto pDSV = DXUTGetD3D11DepthStencilView();
+	pd3dImmediateContext->ClearDepthStencilView(pDSV, D3D11_CLEAR_DEPTH, 1.0, 0);
+
+
+
+	// Render a triangle
+	pd3dImmediateContext->VSSetShader(g_pVertexShader, nullptr, 0);
+	pd3dImmediateContext->PSSetShader(g_pPixelShader, nullptr, 0);
+	pd3dImmediateContext->Draw(3, 0);
+
+
+}
+
+//--------------------------------------------------------------------------------------
+// Release D3D11 resources created in OnD3D11ResizedSwapChain 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D11ReleasingSwapChain(void* pUserContext)
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Release D3D11 resources created in OnD3D11CreateDevice 
+//--------------------------------------------------------------------------------------
+void CALLBACK OnD3D11DestroyDevice(void* pUserContext)
+{	
+	SAFE_RELEASE(g_pVertexBuffer);
+	SAFE_RELEASE(g_pInputLayout);
+	SAFE_RELEASE(g_pPixelShader);
+	SAFE_RELEASE(g_pVertexShader);
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle messages to the application
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK MsgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+	bool* pbNoFurtherProcessing, void* pUserContext)
+{
+	return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle key presses
+//--------------------------------------------------------------------------------------
+void CALLBACK OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext)
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle mouse button presses
+//--------------------------------------------------------------------------------------
+void CALLBACK OnMouse(bool bLeftButtonDown, bool bRightButtonDown, bool bMiddleButtonDown,
+	bool bSideButton1Down, bool bSideButton2Down, int nMouseWheelDelta,
+	int xPos, int yPos, void* pUserContext)
+{
+}
+
+
+//--------------------------------------------------------------------------------------
+// Call if device was removed.  Return true to find a new device, false to quit
+//--------------------------------------------------------------------------------------
+bool CALLBACK OnDeviceRemoved(void* pUserContext)
+{
+	return true;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Initialize everything and go into a render loop
+//--------------------------------------------------------------------------------------
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	// Enable run-time memory check for debug builds.
+#ifdef _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	// DXUT will create and use the best device
+	// that is available on the system depending on which D3D callbacks are set below
+
+	// Set general DXUT callbacks
+	DXUTSetCallbackFrameMove(OnFrameMove);
+	DXUTSetCallbackKeyboard(OnKeyboard);
+	DXUTSetCallbackMouse(OnMouse);
+	DXUTSetCallbackMsgProc(MsgProc);
+	DXUTSetCallbackDeviceChanging(ModifyDeviceSettings);
+	DXUTSetCallbackDeviceRemoved(OnDeviceRemoved);
+
+	// Set the D3D11 DXUT callbacks. Remove these sets if the app doesn't need to support D3D11
+	DXUTSetCallbackD3D11DeviceAcceptable(IsD3D11DeviceAcceptable);
+	DXUTSetCallbackD3D11DeviceCreated(OnD3D11CreateDevice);
+	DXUTSetCallbackD3D11SwapChainResized(OnD3D11ResizedSwapChain);
+	DXUTSetCallbackD3D11FrameRender(OnD3D11FrameRender);
+	DXUTSetCallbackD3D11SwapChainReleasing(OnD3D11ReleasingSwapChain);
+	DXUTSetCallbackD3D11DeviceDestroyed(OnD3D11DestroyDevice);
+
+	// Perform any application-level initialization here
+
+	DXUTInit(true, true, nullptr); // Parse the command line, show msgboxes on error, no extra command line params
+	DXUTSetCursorSettings(true, true); // Show the cursor and clip it when in full screen
+	DXUTCreateWindow(L"DirectXTwo");
+
+	// Only require 10-level hardware or later
+	DXUTCreateDevice(D3D_FEATURE_LEVEL_10_0, true, 800, 600);
+	DXUTMainLoop(); // Enter into the DXUT ren  der loop
+
+					// Perform any application-level cleanup here
+
+	return DXUTGetExitCode();
+}
+
+
