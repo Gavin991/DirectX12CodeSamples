0bc31f3a11212115a258d59e8d783d4276569554
Gavin991 <qinboming991@163.com>
2019-01-24 19:37:58 +0800

    添加DirectX12XAMLAlphaBlend实例

---
+++ DirectX12CodeSamplesSolution/DirectX12XAMLAlphaBlend/DirectX12XAMLAlphaBlend/Content/Sample3DSceneRenderer.h
@@ -0,0 +1,100 @@
+﻿#pragma once
+
+#include "..\Common\DeviceResources.h"
+#include "ShaderStructures.h"
+#include "..\Common\StepTimer.h"
+#include "ConstantBufferViewHeapManager.h"
+#include "CustomSphere.h"
+#include "TextureManager.h"
+#include "ConstantPixelShaderBufferInformation.h"
+#include "CustomCube.h"
+#include "PipelineStateManager.h"
+#include "CustomPlane.h"
+#include "SimpleCamera.h"
+
+namespace DirectX12XAMLAlphaBlend
+{
+	// 此示例呈现器实例化一个基本渲染管道。
+	class Sample3DSceneRenderer
+	{
+	public:
+		Sample3DSceneRenderer(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~Sample3DSceneRenderer();
+		void CreateDeviceDependentResources();
+		void CreateWindowSizeDependentResources();
+		void Update(DX::StepTimer const& timer);
+		bool Render();
+		void SaveState();
+
+		void StartTracking();
+		void TrackingUpdate(float positionX);
+		void StopTracking();
+		bool IsTracking() { return m_tracking; }
+
+		void OnHandleCallBack(bool res);
+		void SetSphereRotateEnable() 
+		{ 
+			m_customSphere.SetRotateEnable();
+		 }
+		void HandleXChanged(float x) {
+			m_constantPSBuffer.HandleXChanged(x); 
+			m_customSphere.HandleXChanged(x);
+		}
+
+		void HandleCameraXChanged(float x) { m_fCameraX = x; m_customSphere.HandleCameraXChanged(x); }//HandleCameraYChanged
+		void HandleCameraYChanged(float y) { m_fCameraY = y; m_customSphere.HandleCameraXChanged(y); }//HandleCameraYChanged
+
+		bool GetIsLoadingComplete() {			return m_loadingComplete;		}
+	private:
+		void LoadState();
+		void Rotate(float radians);
+
+	private:	
+		//相机X轴移动量
+		float m_fCameraX = 0.0f;
+		float m_fCameraY = 0.0f;
+		// 常量缓冲区大小必须都是 256 字节的整数倍。
+		static const UINT c_alignedConstantBufferSize = (sizeof(ModelViewProjectionConstantBuffer) + 255) & ~255;
+
+		// 缓存的设备资源指针。
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+	
+		Microsoft::WRL::ComPtr<ID3D12RootSignature>			m_rootSignature;		
+
+		ConstantBufferViewHeapManager m_cbvHeapManager;
+
+		CustomSphere m_customSphere;
+		CustomCube m_CustomCube;
+		CustomPlane m_customPlane;
+		
+
+		D3D12_RECT											m_scissorRect;
+
+		std::vector<byte>									m_vertexShader;
+		std::vector<byte>									m_pixelShader;
+
+
+		// 用于渲染循环的变量。
+		bool	m_loadingComplete;
+		float	m_radiansPerSecond;
+		float	m_angle;
+		bool	m_tracking;
+		// 用于保存鼠标左键的偏移量。
+		float   m_positionX;
+
+
+		//Microsoft::WRL::ComPtr<ID3D12Resource> m_texture;
+		TextureManager m_textureManager;
+
+		//// 像素着色器常量缓存，它也必须是256字节对其的
+		//// Constant buffers must be 256-byte aligned.
+		ConstantPixelShaderBufferInformation m_constantPSBuffer;
+
+		PipelineStateManager m_pipelineStateManager;
+
+		SimpleCamera             g_SimpleCamera;
+
+		
+	};
+}
+
---
+++ DirectX12CodeSamplesSolution/DirectX12XAMLAlphaBlend/DirectX12XAMLAlphaBlend/MainPage.xaml.cpp
@@ -0,0 +1,280 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage 类的实现。
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace DirectX12XAMLAlphaBlend;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+using namespace concurrency;
+using namespace Windows::Graphics::Display;
+using namespace Windows::System::Threading;
+using namespace Windows::UI::Core;
+//using namespace Windows::UI::Input;
+using Microsoft::WRL::ComPtr;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	// 注册页面生命周期的事件处理程序。
+	CoreWindow^ window = Window::Current->CoreWindow;
+
+	window->VisibilityChanged +=
+		ref new TypedEventHandler<CoreWindow^, VisibilityChangedEventArgs^>(this, &MainPage::OnVisibilityChanged);
+
+	DisplayInformation^ currentDisplayInformation = DisplayInformation::GetForCurrentView();
+
+	currentDisplayInformation->DpiChanged +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &MainPage::OnDpiChanged);
+
+	currentDisplayInformation->OrientationChanged +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &MainPage::OnOrientationChanged);
+
+	DisplayInformation::DisplayContentsInvalidated +=
+		ref new TypedEventHandler<DisplayInformation^, Object^>(this, &MainPage::OnDisplayContentsInvalidated);
+
+	swapChainPanel->CompositionScaleChanged +=
+		ref new TypedEventHandler<SwapChainPanel^, Object^>(this, &MainPage::OnCompositionScaleChanged);
+
+	swapChainPanel->SizeChanged +=
+		ref new SizeChangedEventHandler(this, &MainPage::OnSwapChainPanelSizeChanged);
+
+	// 注册我们的 SwapChainPanel 以获取独立的输入指针事件
+	auto workItemHandler = ref new WorkItemHandler([this](IAsyncAction ^)
+	{
+		//对于指定的设备类型，无论它是在哪个线程上创建的，CoreIndependentInputSource 都将引发指针事件。
+		m_coreInput = swapChainPanel->CreateCoreIndependentInputSource(
+			Windows::UI::Core::CoreInputDeviceTypes::Mouse |
+			Windows::UI::Core::CoreInputDeviceTypes::Touch |
+			Windows::UI::Core::CoreInputDeviceTypes::Pen
+		);
+
+		//指针事件的寄存器，将在后台线程上引发。
+		m_coreInput->PointerPressed += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &MainPage::OnPointerPressed);
+		m_coreInput->PointerMoved += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &MainPage::OnPointerMoved);
+		m_coreInput->PointerReleased += ref new TypedEventHandler<Object^, PointerEventArgs^>(this, &MainPage::OnPointerReleased);
+
+		// 一旦发送输入消息，即开始处理它们。
+		m_coreInput->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+	});
+
+	// 在高优先级的专用后台线程上运行任务。
+	m_inputLoopWorker = ThreadPool::RunAsync(workItemHandler, WorkItemPriority::High, WorkItemOptions::TimeSliced);
+
+	m_main = std::unique_ptr<DirectX12UWPCubeMain>(new DirectX12UWPCubeMain());
+
+	//键盘响应事件
+	Window::Current->CoreWindow->Dispatcher->AcceleratorKeyActivated += ref new TypedEventHandler<Windows::UI::Core::CoreDispatcher^, Windows::UI::Core::AcceleratorKeyEventArgs^>(this, &MainPage::OnDispatcherAcceleratorKeyActivated);
+	//window->Dispatcher.AcceleratorKeyActivated += OnDispatcherAcceleratorKeyActivated; (Windows::UI::Core::CoreDispatcher^ sender, Windows::UI::Core::AcceleratorKeyEventArgs^ args)
+}
+
+
+MainPage::~MainPage()
+{
+	// 析构时停止渲染和处理事件。
+	m_main->StopRenderLoop();
+	m_coreInput->Dispatcher->StopProcessEvents();
+}
+
+
+// 窗口事件处理程序。
+
+void MainPage::OnVisibilityChanged(CoreWindow^ sender, VisibilityChangedEventArgs^ args)
+{
+	m_windowVisible = args->Visible;
+	if (m_windowVisible)
+	{
+		m_main->StartRenderLoop(GetDeviceResources());
+	}
+	else
+	{
+		m_main->StopRenderLoop();
+	}
+}
+
+// DisplayInformation 事件处理程序。
+
+void MainPage::OnDpiChanged(DisplayInformation^ sender, Object^ args)
+{
+	//critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	// 注意: 在此处检索到的 LogicalDpi 值可能与应用的有效 DPI 不匹配
+	// 如果正在针对高分辨率设备对它进行缩放。在 DeviceResources 上设置 DPI 后，
+	// 应始终使用 GetDpi 方法进行检索。
+	// 有关详细信息，请参阅 DeviceResources.cpp。
+	GetDeviceResources()->SetDpi(sender->LogicalDpi);
+	m_main->OnWindowSizeChanged();
+}
+
+void MainPage::OnOrientationChanged(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	GetDeviceResources()->SetCurrentOrientation(sender->CurrentOrientation);
+	m_main->OnWindowSizeChanged();
+}
+
+void MainPage::OnDisplayContentsInvalidated(DisplayInformation^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	GetDeviceResources()->ValidateDevice();
+}
+
+// 在单击应用程序栏按钮时调用。
+void MainPage::AppBarButton_Click(Object^ sender, RoutedEventArgs^ e)
+{
+	// 使用应用程序栏(如果它适合你的应用程序)。设计应用程序栏，
+	// 然后填充事件处理程序(与此类似)。
+}
+
+void MainPage::OnPointerPressed(Object^ sender, PointerEventArgs^ e)
+{
+	// 按下指针时开始跟踪指针移动。
+	m_main->StartTracking();
+}
+
+void MainPage::OnPointerMoved(Object^ sender, PointerEventArgs^ e)
+{
+	// 更新指针跟踪代码。
+	if (m_main->IsTracking())
+	{
+		m_main->TrackingUpdate(e->CurrentPoint->Position.X);
+	}
+}
+
+void MainPage::OnPointerReleased(Object^ sender, PointerEventArgs^ e)
+{
+	//// 释放指针时停止跟踪指针移动。
+	m_main->StopTracking();
+}
+
+void MainPage::OnCompositionScaleChanged(SwapChainPanel^ sender, Object^ args)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	GetDeviceResources()->SetCompositionScale(sender->CompositionScaleX, sender->CompositionScaleY);
+	m_main->OnWindowSizeChanged();
+}
+
+void MainPage::OnSwapChainPanelSizeChanged(Object^ sender, SizeChangedEventArgs^ e)
+{
+	critical_section::scoped_lock lock(m_main->GetCriticalSection());
+	GetDeviceResources()->SetLogicalSize(e->NewSize);
+	m_main->OnWindowSizeChanged();
+}
+
+std::shared_ptr<DX::DeviceResources> MainPage::GetDeviceResources()
+{
+	if (m_deviceResources != nullptr && m_deviceResources->IsDeviceRemoved())
+	{
+		// All references to the existing D3D device must be released before a new device
+		// can be created.
+
+		m_deviceResources = nullptr;
+		m_main->StopRenderLoop();
+		m_main->OnDeviceRemoved();
+
+#if defined(_DEBUG)
+		ComPtr<IDXGIDebug1> dxgiDebug;
+		if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug))))
+		{
+			dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_FLAGS(DXGI_DEBUG_RLO_SUMMARY | DXGI_DEBUG_RLO_IGNORE_INTERNAL));
+		}
+#endif
+	}
+
+	if (m_deviceResources == nullptr)
+	{
+		m_deviceResources = std::make_shared<DX::DeviceResources>();
+		m_deviceResources->SetSwapChainPanel(swapChainPanel);
+		m_main->CreateRenderers(m_deviceResources);
+		m_main->StartRenderLoop(m_deviceResources);
+	}
+	return m_deviceResources;
+}
+
+
+void DirectX12XAMLAlphaBlend::MainPage::AroundOnClick(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	m_main->HandleAroundCheck();
+	soundPlayer->Play();
+}
+
+
+void DirectX12XAMLAlphaBlend::MainPage::HandleXChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs^ e)
+{
+	float changed_x= (float)e->NewValue;
+	if (m_windowVisible)
+	{
+		m_main->HandleXChanged(changed_x);
+	}
+	
+	
+	//value = (int)e.newvalue;
+}
+
+void DirectX12XAMLAlphaBlend::MainPage::OnDispatcherAcceleratorKeyActivated(Windows::UI::Core::CoreDispatcher^ sender, Windows::UI::Core::AcceleratorKeyEventArgs^ args)
+{
+	if (m_windowVisible)
+	{
+		if (args->EventType == Windows::UI::Core::CoreAcceleratorKeyEventType::KeyDown)
+		{
+			switch (args->VirtualKey)
+			{
+			case Windows::System::VirtualKey::Up:
+				//m_main->HandleXChanged(-2);
+				step -= 0.1f;
+				m_main->HandleCameraYChanged(step);
+				break;
+			case Windows::System::VirtualKey::Down:
+				//m_main->HandleXChanged(2);
+				step += 0.1f;
+				m_main->HandleCameraYChanged(step);
+				break;
+			case Windows::System::VirtualKey::Left:
+				//m_main->HandleXChanged(-2);
+				step -= 0.1f;
+				m_main->HandleCameraXChanged(step);
+				break;
+			case Windows::System::VirtualKey::Right:
+				//m_main->HandleXChanged(2);
+				step += 0.1f;
+				m_main->HandleCameraXChanged(step);
+				break;
+			}
+		}
+	}
+}
+
+
+void DirectX12XAMLAlphaBlend::MainPage::ResetButton(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	if (m_windowVisible)
+	{
+		step = 0;
+		m_main->HandleCameraYChanged(step);
+		m_main->HandleCameraXChanged(step);
+		soundPlayer->Play();
+	
+	}
+}
+
+void DirectX12XAMLAlphaBlend::MainPage::MovieEnd(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//mediaPlayer->Stop();
+	//mediaPlayer->Source=nullptr;	
+	mp3Player->Play();
+}
